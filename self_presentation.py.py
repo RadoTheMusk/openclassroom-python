588fecea495a91107fc0d6eda60ba467e1de1f3a
Rado <razafn19@gmail.com>
2025-07-19 18:21:11 +0300

    Ajout des exos OpenClassrooms

---
+++ self_presentation.py.py
@@ -0,0 +1,12 @@
+#leçon 2 bases de python openclassroom (19/07/25)
+
+# Définition des variables
+nom = "Rado"
+age = 16
+
+# Premier affichage du nom et de l'âge
+print (f"Je m'appelle {nom} et j'ai {age} ans")
+
+# Deuxième affichage avec l'âge après 10 ans et nouvelle varaible age
+age = age + 10  #ou age += 10
+print (f"Je m'appelle {nom} et j'ai {age} ans maintenant")
